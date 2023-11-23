8f56ad7a360057f087f06369158e54cad0452758
XiaYue <44867741+XiaYue1013@users.noreply.github.com>
2023-11-23 10:40:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import random
+from flask_frozen import Freezer
+import sys
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 @app.route('/')
 def index():
@@ -9,25 +12,24 @@ def index():
 
 @app.route('/play', methods=['POST'])
 def play():
-    user_choice = request.form['choice']
+    user_choice = request.json['choice']
     choices = ['剪刀', '石頭', '布']
     computer_choice = random.choice(choices)
 
-    result = determine_winner(user_choice, computer_choice)
-
-    return jsonify({'user_choice': user_choice, 'computer_choice': computer_choice, 'result': result})
-
-def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
-        return "平局"
-    elif (
-        (user_choice == '剪刀' and computer_choice == '布') or
-        (user_choice == '石頭' and computer_choice == '剪刀') or
-        (user_choice == '布' and computer_choice == '石頭')
-    ):
-        return "你赢了！"
+        result = "平手!"
+    elif (user_choice == "剪刀" and computer_choice == "布") or \
+         (user_choice == "石頭" and computer_choice == "剪刀") or \
+         (user_choice == "布" and computer_choice == "石頭"):
+        result = "你贏了!"
     else:
-        return "電腦赢了！"
+        result = "你輸了!"
+
+    return jsonify(user_choice=user_choice, computer_choice=computer_choice, result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        print("Building website...")
+        freezer.freeze()
+    else:
+        app.run(debug=True)
